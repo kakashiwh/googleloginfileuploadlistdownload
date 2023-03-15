5eb6badbbe554bb51aed4434287223c650f3da94
kakashiwh <m.rishabhsoni@gmail.com>
2023-03-15 10:38:51 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,138 +1,159 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Dropzone from "react-dropzone";
 import GoogleLogin from "react-google-login";
-import useGoogleAuth from "./useGoogleAuth";
+import {gapi} from "gapi-script";
 
-
-function App() {
-  const REACT_PUBLIC_GOOGLE_CLIENT_ID = process.env.REACT_PUBLIC_GOOGLE_CLIENT_ID;
-  const { user, login, logout } = useGoogleAuth(REACT_PUBLIC_GOOGLE_CLIENT_ID, "email");
-  const [fileList, setFileList] = useState([]);
+const App = () => {
+  const [files, setFiles] = useState([]);
   const [file, setFile] = useState(null);
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+  const  client ="467303493899-gaesmk5jhp5aqbd9mtsfv4mpm9unu8cb.apps.googleusercontent.com";
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: client,
+      });
+    }
 
-  const handleGoogleLogin = (googleUser) => {
-    login(googleUser).then(() => {
-      getFileList();
+    gapi.load('client:auth2', start);
+  }, [client]);
+
+  const fetchData = async () => {
+    const result = await axios("/api/files");
+    setFiles(result.data);
+  };
+
+  const handleDrop = (acceptedFiles) => {
+    setFile(acceptedFiles[0]);
+  };
+
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("user", currentUser);
+
+    try {
+      await axios.post("/api/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      setFile(null);
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDownload = async (filename,uploader) => {
+    if(uploader === currentUser){
+    try {
+      const response = await axios({
+        url: `/api/files/${filename}`,
+        method: "GET",
+        responseType: "blob",
+        params: { email: currentUser.email },
+      });
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", filename);
+      document.body.appendChild(link);
+      link.click();
+    } catch (error) {
+      console.log(error);
+    }
+  }else{
+    console.log("You are not authorized to download this file.");
+  }
+  };
+
+ 
+
+  const handleLogin = (googleUser) => {
+    const id_token = googleUser.getAuthResponse().id_token;
+    fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: `id_token=${encodeURIComponent(id_token)}`
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to authenticate user.");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setLoggedIn(true);
+      setCurrentUser(data.email);
+      fetchData();
+      console.log(data);
+      // handle successful login
+    })
+    .catch((error) => {
+      console.error(error);
+      // handle login error
     });
   };
 
-  const handleGoogleLoginFailure = (error) => {
-    console.error(error);
+  const handleLogout = () => {
+    gapi.auth2.getAuthInstance().signOut().then(() => {
+      setLoggedIn(false);
+      setCurrentUser(null);
+      setFiles([]);
+    });
   };
 
-  const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
-  };
-
-  const handleFileUpload = () => {
-    if (!file) {
-      setErrorMessage("Please select a file.");
-      return;
-    }
-    const formData = new FormData();
-    formData.append("file", file);
-
-    fetch("/api/upload", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${user.token}`,
-      },
-      body: formData,
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to upload file.");
-        }
-        setErrorMessage(null);
-        return response.json();
-      })
-      .then(() => {
-        getFileList();
-      })
-      .catch((error) => {
-        console.error(error);
-        setErrorMessage(error.message);
-      });
-  };
-
-  const getFileList = () => {
-    
-    fetch("/api/list", {
-      headers: {
-        Authorization: `Bearer ${user.token}`,
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to fetch file list.");
-        }
-        setErrorMessage(null);
-        return response.json();
-      })
-      .then((data) => {
-        setFileList(data);
-      })
-      .catch((error) => {
-        console.error(error);
-        setErrorMessage(error.message);
-      });
-  };
-
-  const handleDownload = (file) => {
-    fetch(`/api/list?fileName=${file}`, {
-      headers: {
-        Authorization: `Bearer ${user.token}`,
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to download file.");
-        }
-        return response.blob();
-      })
-      .then((blob) => {
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = file;
-        a.click();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div>
-      {!user ? (
+      {!loggedIn ? (
         <GoogleLogin
-          clientId={REACT_PUBLIC_GOOGLE_CLIENT_ID}
-          buttonText="Sign in with Google"
-          onSuccess={handleGoogleLogin}
-          onFailure={handleGoogleLoginFailure}
-          cookiePolicy={"single_host_origin"}
+          clientId="467303493899-gaesmk5jhp5aqbd9mtsfv4mpm9unu8cb.apps.googleusercontent.com"
+          buttonText="Login with Google"
+          onSuccess={handleLogin}
+          onFailure={(err) => console.log(err)}
         />
       ) : (
         <>
-          <div>
-            <button onClick={logout}>Sign out</button>
-          </div>
-          <div>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={handleFileUpload}>Upload</button>
-            {errorMessage && <p>{errorMessage}</p>}
-          </div>
-          <div>
-            <ul>
-              {fileList.map((file, index) => (
-                <li key={index}>{file} <button onClick={() => handleDownload(file)}>Download</button></li>
+          <h2>Uploaded files:</h2>
+          <button onClick={handleLogout}>Logout</button>
+          <ul>
+              {files.map((file) => (
+              <li key={file.filename}>
+                {file.filename} ({file.uploader})
+                <button onClick={() => handleDownload(file.filename, file.uploader)}>
+                              Download
+                </button>
+              </li>
               ))}
             </ul>
-          </div>
+          <Dropzone onDrop={handleDrop}>
+            {({ getRootProps, getInputProps }) => (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                {file ? (
+                  <p>{file.name}</p>
+                ) : (
+                  <p>
+                    Drag 'n' drop a file here, or click to select a file
+                  </p>
+                )}
+              </div>
+            )}
+          </Dropzone>
+          {file && <button onClick={handleUpload}>Upload</button>}
         </>
       )}
     </div>
   );
-}
+};
 
 export default App;
